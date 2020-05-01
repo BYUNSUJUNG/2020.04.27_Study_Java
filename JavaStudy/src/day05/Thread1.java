d3ad4fa8493b181c1dcf3ac520124809e0994d13
BYUNSUJUNG <susususu07@naver.com>
2020-05-01 19:14:02 +0900

    git init 2020-05-01

---
+++ JavaStudy/src/day05/Thread1.java
@@ -0,0 +1,23 @@
+package day05;
+
+public class Thread1 extends Thread {
+	
+	String data;
+	public Thread1(String data) {
+		this.data = data;
+	}
+	
+	@Override
+	public void run() {
+		for (int i = 0; i < 10; i++) {
+			System.out.println(data);
+			try {
+				sleep(500);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+	
+
+}
