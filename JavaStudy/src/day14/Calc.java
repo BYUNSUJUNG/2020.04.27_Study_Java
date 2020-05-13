d614036d683e71949c5ee30cda3246c099fcab21
BYUNSUJUNG <susususu07@naver.com>
2020-05-13 21:58:48 +0900

    git init 2020-05-13

---
+++ JavaStudy/src/day14/Calc.java
@@ -0,0 +1,28 @@
+package day14;
+
+/**
+ * 
+ * This One is Calculator
+ * @author TED
+ * @since 2019 06 12
+ *
+ */
+public class Calc {
+	/**
+	 *  To add the following numbers. <br>
+	 *  For example<br>
+	 *  add(1,3) return 4
+	 *  
+	 * @param num1: First Integer for add
+	 * @param num2: Last Integer for add
+	 */
+	
+	public static void add(int num1, int num2) {
+		System.out.println(num1+num2);
+	}
+	
+	public static void main(String[] args) {
+		Calc.add(1, 3);
+	}
+
+}
