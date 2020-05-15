caec6487d892d645c3ebdc004c7dfd03674a3e1e
BYUNSUJUNG <susususu07@naver.com>
2020-05-15 21:22:33 +0900

    git init 2020-05-15

---
+++ JavaStudy/src/day16/ButtonTest.java
@@ -0,0 +1,41 @@
+package day16;
+
+import java.awt.Button;
+import java.awt.Frame;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
+public class ButtonTest extends Frame {
+
+	public ButtonTest() {
+		super("익명 클래스 테스트");
+		Button btn = new Button("눌러!");
+		add(btn);
+		setSize(500,500);
+		
+		addWindowListener(new WindowAdapter() {
+			@Override
+			public void windowClosed(WindowEvent e) {
+				System.out.println("프레임 종료");
+				System.exit(0);
+			}
+		});
+		
+		btn.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseReleased(MouseEvent e) {
+				System.out.println("버튼 눌렀어요!");
+			}
+		});
+		
+		setVisible(true);
+	}
+	
+	
+	public static void main(String[] args) {
+		new ButtonTest();
+	}
+
+}
